3967231e03c3fbe54acef08cba6ef5907a43671f
ilyaskoccc <ilyas_1997koc@hotmail.com>
2024-08-11 14:39:35 +0300

    sipariş oluştur componenti başlangıcı ve denemeler

---
+++ components/SiparisOlustur.jsx
@@ -0,0 +1,9 @@
+export default function SiparisOlustur() {
+    return (
+        <>
+            <div className="header">
+                <img src="Assets/Iteration-1-assets/logo.svg" alt="Logo" />
+            </div>
+        </>
+    );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
+import SiparisOlustur from '../components/SiparisOlustur.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>Home Page</p>
+      <SiparisOlustur />
     </>
   )
 }
